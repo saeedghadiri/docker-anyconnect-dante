0cfa19c14429c14b19e538f393267a6f4f88e6d0
Michal Nasiadka <michal.nasiadka@pl.ibm.com>
2017-10-03 17:56:37 +0200

    replaced . with ,

--- scripts/connect.sh
+++ scripts/connect.sh
@@ -6,6 +6,6 @@ sleep 5
 /usr/sbin/danted -f /etc/danted.conf -D
 
 socat -d TCP4-LISTEN:3389,fork TCP4:$JUMP1:3389 &
-socat -d TCP4-LISTEN:3390.fork TCP4:$JUMP2:3389 &
+socat -d TCP4-LISTEN:3390,fork TCP4:$JUMP2:3389 &
 
 /bin/bash
