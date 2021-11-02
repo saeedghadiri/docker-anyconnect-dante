b3b76c2010570decdbeb11ec7fc14e5e1348de92
Saeed Ghadiri <saeed.ghadiri@gmail.com>
2021-11-02 22:11:57 +0330

    add automatic restart feature

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM ubuntu
-RUN apt-get update && apt-get install -y openconnect iptables expect dante-server socat
+RUN apt-get update && apt-get install -y openconnect iptables expect dante-server socat curl
 
 ADD scripts/connect.sh /root
 RUN chmod +x /root/connect.sh
--- scripts/connect.sh
+++ scripts/connect.sh
@@ -1,20 +1,28 @@
 #!/bin/bash
-if [ -z "$CERT" ]
-then
-  echo $ANYCONNECT_PASSWORD|openconnect $ANYCONNECT_SERVER --user=$ANYCONNECT_USER --reconnect-timeout=6000 -b
-else
-  echo $ANYCONNECT_PASSWORD|openconnect $ANYCONNECT_SERVER --user=$ANYCONNECT_USER --servercert=$CERT --reconnect-timeout=6000 -b
-fi
+while true
+do
+	if curl -s CHECK_WEBSITE --output /dev/null -m 10; then
+		echo "we have connection";
+	else
+		echo "we don't have connection";
+		echo "killing";
+		pkill openconnect
+		pkill danted
+		echo $ANYCONNECT_PASSWORD|openconnect $ANYCONNECT_SERVER --user=$ANYCONNECT_USER --servercert=$CERT --http-auth=$ANYCONNECT_AUTH_TYPE --reconnect-timeout=6000 -b
+		sleep 2
+		while ! ip link show | grep -q "tun0"; do
+			echo "Initializing the tunnel"
+			sleep 2
+		done
+		echo "Tunnel is ready!"
 
-while ! ip link show | grep -q "tun0"; do
-    echo "Initializing the tunnel"
-    sleep 2
+		/usr/sbin/danted -f /etc/danted.conf -D
+
+		socat -d TCP4-LISTEN:3389,fork TCP4:$JUMP1:3389 &
+		socat -d TCP4-LISTEN:3390,fork TCP4:$JUMP2:3389 &
+
+	fi
+
+	sleep 120
 done
-echo "Tunnel is ready!"
 
-/usr/sbin/danted -f /etc/danted.conf -D
-
-socat -d TCP4-LISTEN:3389,fork TCP4:$JUMP1:3389 &
-socat -d TCP4-LISTEN:3390,fork TCP4:$JUMP2:3389 &
-
-/bin/bash
